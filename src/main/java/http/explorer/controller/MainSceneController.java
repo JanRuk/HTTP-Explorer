afa855104a877b6deacc9f8dc4a7fc1e084178b2
Janinda Rukshan <janruk91@gmail.com>
2025-01-27 11:33:58 +0530

    Update MainSceneController

--- src/main/java/http/explorer/controller/MainSceneController.java
+++ src/main/java/http/explorer/controller/MainSceneController.java
@@ -12,11 +12,29 @@ public class MainSceneController {
 
     public void initialize() {
         txtAddress.setText("www.google.com");
+        loadWebPage(txtAddress.getText());
+        txtAddress.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                txtAddress.selectAll();
+            }
+        });
 //        wbDisplay.getEngine().load("https://www.google.com");
     }
 
     public void txtAddressOnAction(ActionEvent event) {
+        String url = txtAddress.getText();
 
+        if (url.isBlank()) return;
+        loadWebPage(url);
+    }
 
+    private void loadWebPage(String url) {
+
+        String host = separateHost(url);
+    }
+
+    private String separateHost(String url) {
+        String host = url;
+        return host;
     }
 }
